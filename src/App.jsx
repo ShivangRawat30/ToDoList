144977f5e6c8916b9722156413ecce79caab8f2c
ShivangRawat30 <rawatshivang30@gmail.com>
2023-02-27 20:17:07 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import ToDoList from "./ToDoList";
+
+
+const App = () => {
+  const[inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+  const itemEvents = (event) => {
+    setInputList(event.target.value);
+  };
+  const listOfItems = (events) => {
+    setItems((oldItems) => { 
+      return [...oldItems, inputList];
+    }); 
+    setInputList('');
+  };
+  const deleteItems = (id) => {
+    console.log("deleted");
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
+        return index !== id;
+      });
+      })
+  }
+  return (
+    <>
+    <div className="main_div">
+      <div className="center_div">
+        <br />
+        <h1> ToDo List </h1>
+        <br />
+        <input type="text" placeholder="Add a item" onChange={itemEvents} value={inputList} />
+        <button onClick={listOfItems}> + </button>
+
+        <ol>
+          {/* <li> {inputList} </li> */}
+           { items.map((itemval, index) => {
+              return <ToDoList key = {index} id = {index} text ={itemval} onSelect = {deleteItems}/>
+            })}
+        </ol>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default App;
